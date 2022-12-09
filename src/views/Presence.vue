9d34fc2f6136d88b0fcb092d3984f6e15887f5a4
INGANZAMARUMPU <jonkur96@gmail.com>
2022-12-09 18:55:17 +0200

    created all components scaffold

---
+++ src/views/Presence.vue
@@ -0,0 +1,18 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>URUFATANAMENDE</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+  Kaze m'URUFATANAMENDE
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+
+export default {
+}
+</script>
